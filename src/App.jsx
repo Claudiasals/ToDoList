a5914dd060c6574705d552c138bd83709c1637fe
Claudiasals <salsiniclaudia@gmail.com>
2025-10-27 18:23:58 +0100

    chore: setup initial project and install libraries:
     - Initialized Vite React project - Installed react-router-dom, react-redux, @reduxjs/toolkit - Cleaned up unnecessary files - Created base App.jsx component - Installed tailwindcss, postcss, autoprefixer - Added Tailwind directives to index.css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// CREO IL COMPONENTE PRINCIPALE
+
+const App = () => {
+  return (
+    <>
+    <p> TO DO LIST PROJECT </p>
+    </>
+  )
+}
+
+// ESPORTO IL COMPONENTE
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+// MAIN.JSX SERVE A COLLEGARE REDUX ALLO STORE E AD AVVIARE REACT
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
